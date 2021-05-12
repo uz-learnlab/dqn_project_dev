414d4949262c1b036199cd947844a0f9f00ff898
Ruben Martinez-Cantin <rmcantin@gmail.com>
2021-05-12 11:16:39 +0200

    Pulling code to colab

--- cs285/infrastructure/dqn_utils.py
+++ cs285/infrastructure/dqn_utils.py
@@ -36,58 +36,6 @@ def register_custom_envs():
         )
 
 
-def get_env_kwargs(env_name):
-    if env_name in ['MsPacman-v0', 'PongNoFrameskip-v4']:
-        kwargs = {
-            'learning_starts': 50000,
-            'target_update_freq': 10000,
-            'replay_buffer_size': int(1e6),
-            'num_timesteps': int(2e8),
-            'q_func': create_atari_q_network,
-            'learning_freq': 4,
-            'grad_norm_clipping': 10,
-            'input_shape': (84, 84, 4),
-            'env_wrappers': wrap_deepmind,
-            'frame_history_len': 4,
-            'gamma': 0.99,
-        }
-        kwargs['optimizer_spec'] = atari_optimizer(kwargs['num_timesteps'])
-        kwargs['exploration_schedule'] = atari_exploration_schedule(kwargs['num_timesteps'])
-
-    elif env_name == 'LunarLander-v3':
-        def lunar_empty_wrapper(env):
-            return env
-        kwargs = {
-            'optimizer_spec': lander_optimizer(),
-            'q_func': create_lander_q_network,
-            'replay_buffer_size': 50000,
-            'batch_size': 32,
-            'gamma': 1.00,
-            'learning_starts': 1000,
-            'learning_freq': 1,
-            'frame_history_len': 1,
-            'target_update_freq': 3000,
-            'grad_norm_clipping': 10,
-            'lander': True,
-            'num_timesteps': 500000,
-            'env_wrappers': lunar_empty_wrapper
-        }
-        kwargs['exploration_schedule'] = lander_exploration_schedule(kwargs['num_timesteps'])
-
-    else:
-        raise NotImplementedError
-
-    return kwargs
-
-
-def create_lander_q_network(ob_dim, num_actions):
-    return nn.Sequential(
-        nn.Linear(ob_dim, 64),
-        nn.ReLU(),
-        nn.Linear(64, 64),
-        nn.ReLU(),
-        nn.Linear(64, num_actions),
-    )
 
 class Ipdb(nn.Module):
     def __init__(self):
@@ -103,30 +51,6 @@ class PreprocessAtari(nn.Module):
         return x / 255.
 
 
-def create_atari_q_network(ob_dim, num_actions):
-    return nn.Sequential(
-        PreprocessAtari(),
-        nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4),
-        nn.ReLU(),
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
-        nn.ReLU(),
-        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear(3136, 512),  # 3136 hard-coded based on img size + CNN layers
-        nn.ReLU(),
-        nn.Linear(512, num_actions),
-    )
-
-def atari_exploration_schedule(num_timesteps):
-    return PiecewiseSchedule(
-        [
-            (0, 1.0),
-            (1e6, 0.1),
-            (num_timesteps / 8, 0.01),
-        ], outside_value=0.01
-    )
-
 
 def atari_ram_exploration_schedule(num_timesteps):
     return PiecewiseSchedule(
@@ -137,44 +61,6 @@ def atari_ram_exploration_schedule(num_timesteps):
         ], outside_value=0.01
     )
 
-
-def atari_optimizer(num_timesteps):
-    lr_schedule = PiecewiseSchedule(
-        [
-            (0, 1e-1),
-            (num_timesteps / 40, 1e-1),
-            (num_timesteps / 8, 5e-2),
-        ],
-        outside_value=5e-2,
-    )
-
-    return OptimizerSpec(
-        constructor=optim.Adam,
-        optim_kwargs=dict(
-            lr=1e-3,
-            eps=1e-4
-        ),
-        learning_rate_schedule=lambda t: lr_schedule.value(t),
-    )
-
-
-def lander_optimizer():
-    return OptimizerSpec(
-        constructor=optim.Adam,
-        optim_kwargs=dict(
-            lr=1,
-        ),
-        learning_rate_schedule=lambda epoch: 1e-3,  # keep init learning rate
-    )
-
-
-def lander_exploration_schedule(num_timesteps):
-    return PiecewiseSchedule(
-        [
-            (0, 1),
-            (num_timesteps * 0.1, 0.02),
-        ], outside_value=0.02
-    )
 
 
 def sample_n_unique(sampling_f, n):
